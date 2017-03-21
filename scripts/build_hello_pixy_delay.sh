93e0d138683e703559e3764e7db8129a727f3083
kudooman <kudooman@aim.com>
2017-03-20 19:10:47 -0700

    Update build_hello_pixy_delay.sh

--- scripts/build_hello_pixy_delay.sh
+++ scripts/build_hello_pixy_delay.sh
@@ -3,8 +3,8 @@
 TARGET_BUILD_FOLDER=../build
 
 mkdir $TARGET_BUILD_FOLDER
-mkdir $TARGET_BUILD_FOLDER/hello_pixy_delay
+mkdir $TARGET_BUILD_FOLDER/hello_pixy
 
-cd $TARGET_BUILD_FOLDER/hello_pixy_delay
+cd $TARGET_BUILD_FOLDER/hello_pixy
 cmake ../../src/host/hello_pixy_delay
 make
